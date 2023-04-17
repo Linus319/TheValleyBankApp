08dadeff53ad33443dc0fd4d81969681dbf99816
Thomas <thomas.fagan01@utrgv.edu>
2023-04-17 00:16:40 -0500

    small update to AccountActivity

--- app/src/main/java/com/example/thevalleybankapp/AccountActivity.java
+++ app/src/main/java/com/example/thevalleybankapp/AccountActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AccountActivity extends AppCompatActivity {
@@ -13,6 +14,8 @@ public class AccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+        Button signOutButton = (Button) findViewById(R.id.signOutButton);
+;
         Intent thisIntent = getIntent();
 
         String email = thisIntent.getStringExtra("com.example.thevalleybankapp.accountEmail");
@@ -24,5 +27,11 @@ public class AccountActivity extends AppCompatActivity {
         welcomeText.setText("Welcome, " + MainActivity.AM.getFirstName(email)+ "!");
         checkingBalance.setText(MainActivity.AM.getCheckingBalance(email));
         savingsBalance.setText(MainActivity.AM.getSavingsBalance(email));
+
+        Intent logOutIntent = new Intent(this, MainActivity.class);
+
+        signOutButton.setOnClickListener(v -> {
+            startActivity(logOutIntent);
+        });
     }
 }
