41af9b54d2bc28e2efc0e9aed4744218fd62f09e
Vadan <valnor12@hotmail.com>
2023-04-27 06:37:07 -0500

    Finished convert activity

--- app/src/main/java/com/example/thevalleybankapp/ConvertActivity.java
+++ app/src/main/java/com/example/thevalleybankapp/ConvertActivity.java
@@ -6,33 +6,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.widget.Button;
+import android.view.View;
+import android.widget.*;
 
 public class ConvertActivity extends AppCompatActivity {
 
-    Button goBackButton;
+    Spinner spi1, spi2;
+    EditText txtEntry;
 
-    TextWatcher textWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-
-        }
-    };
+    TextView txtResult;
+    Button Convert, goBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convert_currency);
+
+        txtEntry = findViewById(R.id.fromText);
+        txtResult = findViewById(R.id.toText);
+        spi1 = findViewById(R.id.spFrom);
+        spi2 = findViewById(R.id.spTo);
+        Convert = findViewById(R.id.equal);
+
+        String[] from = {"USD", "MXN"};
+        ArrayAdapter adp1 = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, from);
+        spi1.setAdapter(adp1);
+
+        String[] to = {"USD", "MXN"};
+        ArrayAdapter adp2 = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, to);
+        spi2.setAdapter(adp2);
+
+        Convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Double conv;
+
+                Double fromAmt = Double.parseDouble(txtEntry.getText().toString());
+
+                if ((spi1.getSelectedItem().toString() == "USD" && spi2.getSelectedItem().toString() == "USD") || (spi1.getSelectedItem().toString() == "MXN" && spi2.getSelectedItem().toString() == "MXN")){
+                    conv = fromAmt;
+                    txtResult.setText("$ "+conv);
+                }
+
+                else if (spi1.getSelectedItem().toString() == "MXN" && spi2.getSelectedItem().toString() == "USD"){
+                    conv = fromAmt / 18.16;
+                    txtResult.setText("$ " + String.format("%.2f", conv));
+                }
+
+                else if (spi1.getSelectedItem().toString() == "USD" && spi2.getSelectedItem().toString() == "MXN"){
+                    conv = fromAmt * 18.16;
+                    txtResult.setText("$ " + String.format("%.2f", conv));
+                }
+
+            }
+        });
 
         Intent thisIntent = getIntent();
 
@@ -46,5 +74,28 @@ public class ConvertActivity extends AppCompatActivity {
             goBackIntent.putExtra("com.example.thevalleybankapp.accountEmail", email);
             startActivity(goBackIntent);
         });
+
     }
+
+//    TextWatcher textWatcher = new TextWatcher() {
+//        @Override
+//        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//        }
+//
+//        @Override
+//        public void onTextChanged(CharSequence s, int start, int before, int count) {
+//            if (txtEntry.getText().toString().equals("")) {
+//                return;
+//            }
+//
+//            double money = Double.parseDouble(txtEntry.getText().toString());
+//        }
+//
+//        @Override
+//        public void afterTextChanged (Editable s){
+//
+//        }
+//
+//    };
+
 }
